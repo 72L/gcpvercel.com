fc60c1b7cb57fc3301aabad199d8f3cdd666dbac
Fazil Akhtar <2883697+fazilakhtar@users.noreply.github.com>
2024-05-26 11:35:57 +0300

    fix: update example code installation step
    - updated the code sample to the updated environment variables that are
      added to the Vercel project during installation

--- app/install/codeSamples/page.tsx
+++ app/install/codeSamples/page.tsx
@@ -39,11 +39,11 @@ export default function Page() {
           {`
 export const getGCPCredentials = () => {
   // for Vercel, use environment variables
-  return process.env.GOOGLE_PRIVATE_KEY
+  return process.env.GCP_PRIVATE_KEY
     ? {
         credentials: {
-          client_email: process.env.GCLOUD_SERVICE_ACCOUNT_EMAIL,
-          private_key: process.env.GOOGLE_PRIVATE_KEY,
+          client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+          private_key: process.env.GCP_PRIVATE_KEY,
         },
         projectId: process.env.GCP_PROJECT_ID,
       }
